c360376c315d673b4deeda149bf9f8c48b588969
İbrahim Basan <115656656+d4ze@users.noreply.github.com>
2024-03-15 03:37:59 +0300

    Create BasicUsernameAndPasswordCheck.cs

---
+++ Basic C# Pojects/BasicUsernameAndPasswordCheck.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Username:");
+string username = Console.ReadLine();
+Console.Write("Password:");
+string password = Console.ReadLine();
+
+
+if (username == "admin" && password == "1234")
+{
+    Console.WriteLine("Login is successful");
+}
+else
+{
+    Console.WriteLine("Wrong username or password");
+}
