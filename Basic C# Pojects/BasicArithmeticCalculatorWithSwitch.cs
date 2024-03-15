ea6bfee1c3dcf218d2ddccc516f7deb38d577d08
İbrahim Basan <115656656+d4ze@users.noreply.github.com>
2024-03-15 03:25:59 +0300

    Create BasicArithmeticCalculatorWithSwitch.cs

---
+++ Basic C# Pojects/BasicArithmeticCalculatorWithSwitch.cs
@@ -0,0 +1,45 @@
+﻿string operators;
+double number1, number2;
+double result = 0;
+
+Console.Write("Number 1:");
+number1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Operator:");
+operators = Console.ReadLine();
+Console.Write("Number 2:");
+number2 = Convert.ToDouble(Console.ReadLine());
+
+
+
+
+switch (operators)
+{
+    case "+":
+        result = number1 + number2;
+        Console.WriteLine(number1 + " " + operators + " " + number2 + " = " + result);
+
+
+        break;
+    case "-":
+        result = number1 - number2;
+        Console.WriteLine(number1 + " " + operators + " " + number2 + " = " + result);
+        break;
+    case "*":
+        result = number1 * number2;
+        Console.WriteLine(number1 + " " + operators + " " + number2 + " = " + result);
+        break;
+
+    case "/":
+        if (number2 == 0)
+        {
+            Console.WriteLine("Cannot divide by zero.");
+            break;
+        }
+        result = number1 / number2;
+        Console.WriteLine(number1 + " " + operators + " " + number2 + " = " + result);
+        break;
+
+    default:
+        Console.WriteLine("Operator is not found");
+        break;
+}
